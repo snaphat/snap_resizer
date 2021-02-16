da3774a2b72a238514a5294d3a52830010861dd7
Aaron Landwehr <snaphat@gmail.com>
2021-02-15 21:17:19 -0500

    Add EnumWindows callback hook.

--- src/main.rs
+++ src/main.rs
@@ -32,26 +32,42 @@ fn _msgbox(title: &str, msg: &str) -> Result<i32, Error> {
     }
 }
 
-extern "system" fn callback(hWinEventHook: HWINEVENTHOOK, event: DWORD, hwnd: HWND, idObject: LONG, idChild: LONG, idEventThread: DWORD, dwmsEventTime: DWORD) {
-    if event == EVENT_SYSTEM_MOVESIZEEND && idChild == 0 {
-        let rect = unsafe {
+struct State {
+    hwnd: HWND,
+    rect: RECT,
+}
+
+extern "system" fn enum_windows_callback(hwnd: HWND, state: LPARAM) -> BOOL
+{
+    println!("Inside addr: {:?}", state);
+    let state: &State = unsafe { mem::transmute(state) };
+    println!("{:?}", hwnd); //hwnd of enumerated window.
+    let rect = state.rect;
+    let moved_hwnd = state.hwnd;
+    println!("(x,y)\nhandle: {:?}\nStart: ({}, {})\nStop: ({}, {})", moved_hwnd, rect.left, rect.top, rect.right, rect.bottom);
+    1 // Return true to continue enumerating
+}
+
+extern "system" fn set_win_event_hook_callback(_: HWINEVENTHOOK, event: DWORD, hwnd: HWND, _: LONG, id_child: LONG, _: DWORD, _: DWORD) {
+    if event == EVENT_SYSTEM_MOVESIZEEND && id_child == 0 {
+        unsafe {
             let rect: RECT = mem::zeroed();
             let ptr: LPVOID = mem::transmute(&rect);
             if DwmGetWindowAttribute(hwnd, DWMWA_EXTENDED_FRAME_BOUNDS, ptr, mem::size_of_val(&rect) as u32) != S_OK {
                 msgbox!("Err", "Failed to get rect!");
-                None
             } else {
-                Some(rect)
+                let state = State{hwnd, rect};
+                let state: LPARAM = mem::transmute(&state);
+                let callback:WNDENUMPROC = Some(enum_windows_callback);
+                println!("Outside addr: {:?}", state);
+                EnumWindows(callback, state);
             }
         };
-        if let Some(rect) = rect {
-            msgbox!("(x,y)", "\nStart: ({}, {})\nStop: ({}, {})", rect.left, rect.top, rect.right, rect.bottom);
-        }
     }
 }
 
 fn main() {
-    let callback: WINEVENTPROC = Some(callback);
+    let callback: WINEVENTPROC = Some(set_win_event_hook_callback);
     let hook = unsafe {
         SetWinEventHook(
             EVENT_SYSTEM_MOVESIZEEND,
