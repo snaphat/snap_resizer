c504185d020ad5e06b170231fd560a683c0cb381
Aaron Landwehr <snaphat@gmail.com>
2021-02-18 01:53:31 -0500

    Implement safe APIs for winapis. Add ability to pass closures to enum_windows() API. Remove transmutes.

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,73 @@
 #[cfg(windows)]
 extern crate winapi;
-use std::io::Error;
-use std::mem;
-use std::ptr;
+mod win_wrapper;
+use win_wrapper::*;
 use winapi::shared::minwindef::*;
 use winapi::shared::windef::*;
-use winapi::shared::winerror::*;
-use winapi::um::dwmapi::*;
 use winapi::um::winnt::*;
 use winapi::um::winuser::*;
 
-macro_rules! msgbox {
-    ($title:tt, $($arg:tt)*) => ({
-        let res = format!($($arg)*);
-        _msgbox($title, &res).unwrap()
-    })
+macro_rules! unwrap_or_return {
+    ( $e:expr ) => {
+        match $e {
+            | Some(x) => x,
+            | _ => return,
+        }
+    };
 }
 
-fn _msgbox(title: &str, msg: &str) -> Result<i32, Error> {
-    use std::ffi::OsStr;
-    use std::iter::once;
-    use std::os::windows::ffi::OsStrExt;
-    let title: Vec<u16> = OsStr::new(title).encode_wide().chain(once(0)).collect();
-    let msg: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
-    let ret = unsafe { MessageBoxW(ptr::null_mut(), msg.as_ptr(), title.as_ptr(), MB_OK) };
-
-    if ret == 0 {
-        Err(Error::last_os_error())
-    } else {
-        Ok(ret)
-    }
-}
-
-struct State {
+extern "system" fn set_win_event_hook_callback(
+    _: HWINEVENTHOOK,
+    event: DWORD,
     hwnd: HWND,
-    rect: RECT,
-}
-
-extern "system" fn enum_windows_callback(hwnd: HWND, state: LPARAM) -> BOOL
-{
-    println!("Inside addr: {:?}", state);
-    let state: &State = unsafe { mem::transmute(state) };
-    println!("{:?}", hwnd); //hwnd of enumerated window.
-    let rect = state.rect;
-    let moved_hwnd = state.hwnd;
-    println!("(x,y)\nhandle: {:?}\nStart: ({}, {})\nStop: ({}, {})", moved_hwnd, rect.left, rect.top, rect.right, rect.bottom);
-    1 // Return true to continue enumerating
-}
-
-extern "system" fn set_win_event_hook_callback(_: HWINEVENTHOOK, event: DWORD, hwnd: HWND, _: LONG, id_child: LONG, _: DWORD, _: DWORD) {
-    if event == EVENT_SYSTEM_MOVESIZEEND && id_child == 0 {
-        unsafe {
-            let rect: RECT = mem::zeroed();
-            let ptr: LPVOID = mem::transmute(&rect);
-            if DwmGetWindowAttribute(hwnd, DWMWA_EXTENDED_FRAME_BOUNDS, ptr, mem::size_of_val(&rect) as u32) != S_OK {
-                msgbox!("Err", "Failed to get rect!");
-            } else {
-                let state = State{hwnd, rect};
-                let state: LPARAM = mem::transmute(&state);
-                let callback:WNDENUMPROC = Some(enum_windows_callback);
-                println!("Outside addr: {:?}", state);
-                EnumWindows(callback, state);
-            }
-        };
+    _: LONG,
+    id_child: LONG,
+    _: DWORD,
+    _: DWORD,
+) {
+    if event != EVENT_SYSTEM_MOVESIZEEND || id_child != 0 {
+        return;
     }
+    println!("Move event...");
+    let rect = unwrap_or_return!(get_window_bounds(hwnd));
+
+    // Enum handler
+    let enum_closure = |o_hwnd: HWND| -> i32 {
+        if !is_window_visible(o_hwnd) {
+            return 1;
+        }
+
+        if let Some(o_rect) = get_window_bounds(o_hwnd) {
+            println!(
+                "(x,y)\nhandle: {:?}\nStart: ({}, {})\nStop: ({}, {})",
+                hwnd, rect.left, rect.top, rect.right, rect.bottom
+            );
+            println!(
+                "\n(x,y)\nhandle: {:?}\nStart: ({}, {})\nStop: ({}, {})",
+                o_hwnd, o_rect.left, o_rect.top, o_rect.right, o_rect.bottom
+            );
+            println!("==========================");
+        }
+
+        return 1;
+    };
+    enum_windows(enum_closure);
 }
 
 fn main() {
-    let callback: WINEVENTPROC = Some(set_win_event_hook_callback);
-    let hook = unsafe {
-        SetWinEventHook(
-            EVENT_SYSTEM_MOVESIZEEND,
-            EVENT_SYSTEM_MOVESIZEEND,
-            ptr::null_mut(),
-            callback,
-            0,
-            0,
-            WINEVENT_OUTOFCONTEXT,
-        )
-    };
-    if hook.is_null() {
+    if set_win_event_hook(set_win_event_hook_callback) == false {
         msgbox!("Err", "Failed to set move/resize event hook!");
         return;
     }
 
     loop {
-        unsafe {
-            let mut message: MSG = mem::zeroed();
-            if GetMessageW(&mut message as *mut MSG, ptr::null_mut(), 0, 0) > 0 {
-                TranslateMessage(&message as *const MSG);
-                DispatchMessageW(&message as *const MSG);
-            } else {
-                break;
+        let msg = get_message();
+        match msg {
+            | Some(msg) => {
+                translate_message(&msg);
+                dispatch_message(&msg);
             }
-        }
+            | _ => break,
+        };
     }
 }
---
+++ src/win_wrapper.rs
@@ -0,0 +1,129 @@
+#![macro_use]
+use std::io::Error;
+use std::mem;
+use std::ptr;
+use winapi::shared::minwindef::*;
+use winapi::shared::windef::*;
+use winapi::shared::winerror::*;
+use winapi::um::dwmapi::*;
+use winapi::um::winnt::*;
+use winapi::um::winuser::*;
+
+pub fn get_message() -> Option<MSG> {
+    let mut msg: MSG = unsafe { mem::MaybeUninit::zeroed().assume_init() };
+    let ret = unsafe { GetMessageW(&mut msg as *mut MSG, ptr::null_mut(), 0, 0) };
+    match ret {
+        | 0 => {
+            println!("Error in get_message() return.");
+            None
+        }
+        | -1 => None,
+        | _ => Some(msg),
+    }
+}
+
+pub fn translate_message(msg: *const MSG) {
+    unsafe { TranslateMessage(msg) };
+}
+
+pub fn dispatch_message(msg: *const MSG) {
+    unsafe { DispatchMessageW(msg) };
+}
+
+macro_rules! msgbox {
+    ($title:tt, $($arg:tt)*) => ({
+        let res = format!($($arg)*);
+        _msgbox($title, &res).unwrap()
+    })
+}
+
+pub fn _msgbox(title: &str, msg: &str) -> Result<i32, Error> {
+    use std::ffi::OsStr;
+    use std::iter::once;
+    use std::os::windows::ffi::OsStrExt;
+    let title: Vec<u16> = OsStr::new(title).encode_wide().chain(once(0)).collect();
+    let msg: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
+    let ret = unsafe { MessageBoxW(ptr::null_mut(), msg.as_ptr(), title.as_ptr(), MB_OK) };
+
+    match ret {
+        | 0 => Err(Error::last_os_error()),
+        | _ => Ok(ret),
+    }
+}
+
+pub fn is_window_visible(hwnd: HWND) -> bool {
+    return unsafe { IsWindowVisible(hwnd) } != 0;
+}
+
+pub fn get_window_bounds(hwnd: HWND) -> Option<RECT> {
+    let (ret, rect) = unsafe {
+        let mut _rect = mem::MaybeUninit::<RECT>::zeroed().assume_init();
+        let ptr = &_rect as *const RECT as LPVOID;
+        let ret = DwmGetWindowAttribute(
+            hwnd,
+            DWMWA_EXTENDED_FRAME_BOUNDS,
+            ptr,
+            mem::size_of_val(&_rect) as u32,
+        );
+        (ret, _rect)
+    };
+    match ret {
+        | S_OK => Some(rect),
+        | _ => {
+            println!("Failed to get rect! for handle {:?}", hwnd);
+            None
+        }
+    }
+}
+
+pub fn enum_windows<F>(func: F)
+where
+    F: Fn(HWND) -> i32,
+{
+    // Implement trait to pass closure as lparam.
+    // See: https://stackoverflow.com/questions/38995701/how-do-i-pass-a-closure-through-raw-pointers-as-an-argument-to-a-c-function
+    let trait_obj: &dyn Fn(HWND) -> i32 = &func;
+    let trait_obj_ref = &trait_obj;
+    let state = trait_obj_ref as *const _ as LPARAM;
+
+    // Wrapper function to call closure.
+    extern "system" fn enum_windows_callback(hwnd: HWND, lparam: LPARAM) -> BOOL {
+        let trait_obj_ref: &&dyn Fn(HWND) -> i32 = unsafe {
+            let closure_pointer_pointer = lparam as *const LPARAM;
+            &*(closure_pointer_pointer as *const _)
+        };
+        return trait_obj_ref(hwnd);
+    }
+
+    // Run Callback API.
+    let callback: WNDENUMPROC = Some(enum_windows_callback);
+    unsafe {
+        EnumWindows(callback, state);
+    }
+}
+
+type EventHookFunc = extern "system" fn(
+    _: HWINEVENTHOOK,
+    event: DWORD,
+    hwnd: HWND,
+    _: LONG,
+    id_child: LONG,
+    _: DWORD,
+    _: DWORD,
+);
+
+pub fn set_win_event_hook(cb: EventHookFunc) -> bool {
+    let cb: WINEVENTPROC = Some(cb);
+    let hook = unsafe {
+        SetWinEventHook(
+            EVENT_SYSTEM_MOVESIZEEND,
+            EVENT_SYSTEM_MOVESIZEEND,
+            ptr::null_mut(),
+            cb,
+            0,
+            0,
+            WINEVENT_OUTOFCONTEXT,
+        )
+    };
+    !hook.is_null()
+}
